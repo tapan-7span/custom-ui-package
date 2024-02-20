e9c329f1dc60db01eb34f13d7a63053ed5589afe
tapan-7span <tapan@7span.com>
2024-02-20 14:22:39 +0530

    setup workspace

--- apps/vite-vue/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from "vue";
-import "./style.css";
-import App from "./App.vue";
-import VueDatePicker from "@vuepic/vue-datepicker";
-import "@vuepic/vue-datepicker/dist/main.css";
-
-const app = createApp(App);
-app.component("VueDatePicker", VueDatePicker);
-app.mount("#app");
