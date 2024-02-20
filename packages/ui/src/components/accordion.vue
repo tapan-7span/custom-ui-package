c796f38dd62357a911949305dc89d2c6c2066399
tapan-7span <tapan@7span.com>
2024-02-20 15:27:58 +0530

    accordion component

---
+++ packages/ui/src/components/accordion.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="bg-white rounded-md p-1 transition-all duration-700">
+    <button
+      @click="isToggle = !isToggle"
+      class="text-xl flex items-center justify-between w-full"
+    >
+      <label>{{ title }}</label>
+      <Arrow
+        class="transition-all duration-700"
+        :class="isToggle ? 'rotate-90' : '-rotate-90'"
+      />
+    </button>
+    <div v-if="isToggle"><slot /></div>
+  </div>
+</template>
+<script setup>
+import { ref } from "vue";
+import Arrow from "./icons/arrow.vue";
+const props = defineProps({
+  title: String,
+});
+const isToggle = ref(false);
+</script>
