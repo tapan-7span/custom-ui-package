49199f092c40407cbcbd610262d9d6cfce01c7c3
tapan-7span <tapan@7span.com>
2024-02-21 16:24:28 +0530

    nuxt app init

---
+++ apps/nuxt-vue/app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex items-center justify-center h-screen bg-neutral-100 w-full">
+    <div class="container w-full px-48 ">
+      <!-- <Accordion class="shadow-2xl border border-black" title="Title">
+        <template #default> description </template>
+      </Accordion> -->
+    </div>
+  </div>
+</template>
+<script setup></script>
---
+++ apps/nuxt-vue/plugins/ui-components.js
@@ -0,0 +1,6 @@
+// import UiComponents from "../../../packages/ui/dist/ui-components";
+// import "../../../packages/ui/dist/style.css";
+
+export default defineNuxtPlugin((nuxtApp) => {
+  //   nuxtApp.vueApp.use(UiComponents);
+});
